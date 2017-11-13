d059153cf53cf915de046395371c8fdd55e63729
wangyunqiang@qtec.cn <wangyunqiang@qtec.cn>
2017-11-13 10:07:47 +0800

    修改成线程模式

--- app/main.c
+++ app/main.c
@@ -7,12 +7,22 @@
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <linux/netlink.h>
+#include <pthread.h>
 
 #include "logger.h"
 
 #define NETLINK_TEST 17
 #define MAX_PAYLOAD 1024
 #define BUF_SIZE 1024
+
+pthread_mutex_t mutex;
+pthread_cond_t cond;
+int sockfd;
+
+struct sockaddr_nl nladdr;
+struct msghdr msg;
+struct iovec iov;
+struct nlmsghdr *nlh;
 
 static void dump_nlmsg(struct nlmsghdr *nlh)
 {
@@ -21,7 +31,7 @@ static void dump_nlmsg(struct nlmsghdr *nlh)
 	int col = 16;
 	int datalen = NLMSG_PAYLOAD(nlh, 0);
 
-	printf("===============DEBUG START===============\n");
+	printf("User Debug Start===============\n");
 	printf("nlmsghdr info (%d):\n", NLMSG_HDRLEN);
 	printf("  nlmsg_len\t= %d\n" "  nlmsg_type\t= %d\n"
 		"  nlmsg_flags\t= %d\n" "  nlmsg_seq\t= %d\n" "  nlmsg_pid\t= %d\n",
@@ -53,47 +63,120 @@ static void dump_nlmsg(struct nlmsghdr *nlh)
 		}
 		printf("\n");
 	}
-	printf("===============DEBUG END===============\n");
+	printf("User Debug End===============\n");
 }
 
-static int init_netlink_socket(int protocol)
-{
-	int sockfd;
-	struct sockaddr_nl nladdr;
-	int ret;
 
-	sockfd = socket(AF_NETLINK, SOCK_RAW, protocol);
-	if (sockfd < 0) {
-		LOG_ERROR("socket: %s", strerror(errno));
-		return -1;
+void *send_thread(void *arg)
+{
+	int ret;
+    pthread_cleanup_push(pthread_mutex_unlock,&mutex);
+	unsigned long int id1;
+	id1=pthread_self(); 
+	while(1)
+	{
+        printf("send_thread is running id =%d\n",id1);
+        pthread_mutex_lock(&mutex);
+        pthread_cond_wait(&cond,&mutex);
+        printf("send_thread applied the condition\n");
+		
+		ret = send_msg_once(sockfd, &defaultConfig);
+		if (ret < 0) 
+		{
+			LOG_ERROR("send_msg_once: send msg failed");
+			close(sockfd);
+			return EXIT_FAILURE;
+		}
+		if (ret < 0) 
+		{
+			LOG_ERROR("send_msg_once: send msg failed");
+			close(sockfd);
+			return EXIT_FAILURE;
+		}
+        pthread_mutex_unlock(&mutex);
+        sleep(4);
 	}
+    pthread_cleanup_pop(0);
+}
+
+void test_reset_msg(void)
+{
+	memset(&iov, 0, sizeof(iov));
+	iov.iov_base = nlh;
+	iov.iov_len = NLMSG_SPACE(MAX_PAYLOAD);
 
 	memset(&nladdr, 0, sizeof(nladdr));
 	nladdr.nl_family = AF_NETLINK;
-	nladdr.nl_pid = getpid();
+	nladdr.nl_pid = 0;
 	nladdr.nl_groups = 0;
 
-	ret = bind(sockfd, (struct sockaddr *) &nladdr, sizeof(nladdr));
-	if (ret < 0) {
-		LOG_ERROR("bind: %s", strerror(errno));
-		close(sockfd);
+	memset(&msg, 0, sizeof(msg));
+	msg.msg_name = &nladdr;
+	msg.msg_namelen = sizeof(nladdr);
+	msg.msg_iov = &iov;
+	msg.msg_iovlen = 1;
+
+}
+
+int test_recv_msg(int sockfd, char *buf, int len)
+{
+	int ret;
+	LOG_INFO("start to recv message");
+
+	ret = recvmsg(sockfd, (struct msghdr *) &msg, 0);
+	if (ret < 0)
+	{
+		LOG_ERROR("recvmsg: %s", strerror(errno));
 		return ret;
 	}
 
-	return sockfd;
+	strncpy(buf, NLMSG_DATA(nlh), len - 1);
+	buf[len] = '\0';
+
+	dump_nlmsg(nlh);
+
+	return ret;
 }
 
-static int test_send_msg(int sockfd, const char *str)
+void *rcv_thread(void *arg)
+{
+	int ret;
+	char buf[BUF_SIZE];
+	unsigned long int id3;
+	id3=pthread_self();
+    while(1)
+	{	
+		sleep(13);
+        printf("rcv_thread is running id =%d\n",id3);
+        pthread_mutex_lock(&mutex);
+        pthread_cond_wait(&cond,&mutex);
+        printf("rcv_thread applied the condition\n");
+		test_reset_msg();
+		ret = test_recv_msg(sockfd, buf, sizeof(buf));
+		printf("rcv_thread test_recv_msg.....\n");
+		if (ret < 0) 
+		{
+			LOG_ERROR("send_msg_once: send key failed");
+			close(sockfd);
+			return EXIT_FAILURE;
+		}
+        pthread_mutex_unlock(&mutex);
+		
+    }
+}
+
+
+int send_msg_once(int sockfd, struct config *str)
 {
 	struct sockaddr_nl nladdr;
 	struct msghdr msg;
 	struct iovec iov;
 	struct nlmsghdr *nlh;
 	int ret;
-	int len = strlen(str) + 1;
-
+	int len = sizeof(*str);
 	nlh = calloc(1, NLMSG_SPACE(len));
-	if (!nlh) {
+	if (!nlh) 
+	{
 		LOG_ERROR("calloc: alloc nlmsghdr error");
 		return -1;
 	}
@@ -102,7 +185,7 @@ static int test_send_msg(int sockfd, const char *str)
 	nlh->nlmsg_flags = NLM_F_REQUEST;
 	nlh->nlmsg_seq = 9527;
 	nlh->nlmsg_pid = getpid();
-	strcpy(NLMSG_DATA(nlh), str);
+	memcpy(NLMSG_DATA(nlh), str,len);
 
 	memset(&iov, 0, sizeof(iov));
 	iov.iov_base = nlh;
@@ -128,90 +211,85 @@ static int test_send_msg(int sockfd, const char *str)
 		return ret;
 	}
 
-	/* debug info */
 	dump_nlmsg(nlh);
 
 	free(nlh);
 	return ret;
 }
 
-static int test_recv_msg(int sockfd, char *buf, int len)
+int init_netlink_socket(int protocol)
 {
+	int sockfd;
 	struct sockaddr_nl nladdr;
-	struct msghdr msg;
-	struct iovec iov;
-	struct nlmsghdr *nlh;
 	int ret;
 
-	nlh = calloc(1, NLMSG_SPACE(MAX_PAYLOAD));
-	if (!nlh) {
-		LOG_ERROR("calloc: alloc nlmsghdr error");
+	sockfd = socket(AF_NETLINK, SOCK_RAW, protocol);
+	if (sockfd < 0) 
+	{
+		LOG_ERROR("socket: %s", strerror(errno));
 		return -1;
 	}
 
-	memset(&iov, 0, sizeof(iov));
-	iov.iov_base = nlh;
-	iov.iov_len = NLMSG_SPACE(MAX_PAYLOAD);
-
 	memset(&nladdr, 0, sizeof(nladdr));
 	nladdr.nl_family = AF_NETLINK;
-	nladdr.nl_pid = 0;
+	nladdr.nl_pid = getpid();
 	nladdr.nl_groups = 0;
 
-	memset(&msg, 0, sizeof(msg));
-	msg.msg_name = &nladdr;
-	msg.msg_namelen = sizeof(nladdr);
-	msg.msg_iov = &iov;
-	msg.msg_iovlen = 1;
-
-	LOG_INFO("start to recv message");
-
-	ret = recvmsg(sockfd, (struct msghdr *) &msg, 0);
-	if (ret < 0) {
-		LOG_ERROR("recvmsg: %s", strerror(errno));
-		free(nlh);
+	ret = bind(sockfd, (struct sockaddr *) &nladdr, sizeof(nladdr));
+	if (ret < 0)
+	{
+		LOG_ERROR("bind: %s", strerror(errno));
+		close(sockfd);
 		return ret;
 	}
 
-	strncpy(buf, NLMSG_DATA(nlh), len - 1);
-	buf[len] = '\0';
-
-	/* debug info */
-	dump_nlmsg(nlh);
-
-	free(nlh);
-	return ret;
+	return sockfd;
 }
 
 int main()
 {
-	int sockfd;
 	int ret;
 	char buf[BUF_SIZE];
-	const char *str = "Hello Netlink!";
-
 	sockfd = init_netlink_socket(NETLINK_TEST);
-	if (sockfd < 0) {
+	if (sockfd < 0) 
+	{
 		LOG_ERROR("init_netlink_socket: couldn't init netlink socket");
 		return EXIT_FAILURE;
 	}
-
-	ret = test_send_msg(sockfd, str);
-	if (ret < 0) {
-		LOG_ERROR("test_send_msg: send msg failed");
+	ret = send_msg_once(sockfd, &defaultConfig);
+	if (ret < 0) 
+	{
+		LOG_ERROR("send_msg_once: send msg failed");
 		close(sockfd);
 		return EXIT_FAILURE;
 	}
-
+	nlh = calloc(1, NLMSG_SPACE(MAX_PAYLOAD));
+	if (!nlh) 
+	{
+		LOG_ERROR("calloc: alloc nlmsghdr error");
+		return EXIT_FAILURE;
+	}
+	ret = test_reset_msg();
 	ret = test_recv_msg(sockfd, buf, sizeof(buf));
-	if (ret < 0) {
+	if (ret < 0) 
+	{
 		LOG_ERROR("test_recv_msg: recv msg failed");
 		close(sockfd);
 		return EXIT_FAILURE;
 	}
-
 	LOG_INFO("receive from kernel: %s", buf);
-
+	pthread_t thid1,thid2,thid3;
+    printf("condition variable study!\n");
+    pthread_mutex_init(&mutex,NULL);
+    pthread_cond_init(&cond,NULL);
+    pthread_create(&thid1,NULL,(void*)send_thread,NULL);
+	pthread_create(&thid3,NULL,(void*)rcv_thread,NULL);
+	do{
+        pthread_cond_signal(&cond);
+    }while(1);
+    sleep(20);
+	free(nlh);
+    pthread_exit(0);
 	close(sockfd);
 	return EXIT_SUCCESS;
 }
